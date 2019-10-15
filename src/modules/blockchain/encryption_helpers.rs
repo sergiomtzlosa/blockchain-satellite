06b4446a746db40673687e919b3fbd1d7f99411d
Sergio Martinez <sergiomtz.losa@gmail.com>
2019-10-15 18:40:34 +0200

    encryption test

--- src/modules/blockchain/encryption_helpers.rs
+++ src/modules/blockchain/encryption_helpers.rs
@@ -69,6 +69,7 @@ pub fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetricc
     // us that it stopped processing data due to not having any more data in the
     // input buffer.
     loop {
+        
         let result = encryptor.encrypt(&mut read_buffer, &mut write_buffer, true);
 
         // "write_buffer.take_read_buffer().take_remaining()" means:
