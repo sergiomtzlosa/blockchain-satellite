8fdccd4a4dac423dd6b3492e76f634975ad97487
Sergio Martinez <sergiomtz.losa@gmail.com>
2019-10-15 07:10:43 +0200

    leave encryption

--- src/modules/blockchain/encryption.rs
+++ src/modules/blockchain/encryption.rs
@@ -1,17 +1,19 @@
 extern crate openssl;
 
-use openssl::symm::{encrypt, decrypt, Cipher};
+// use openssl::symm::{encrypt, decrypt, Cipher};
 use rustc_serialize::json;
 
 pub use crate::utils;
 
 static FILE_SEED: &str = "/etc/passwd";
 
+#[allow(dead_code)]
 fn get_key() -> String {
 
     return utils::md5_file(FILE_SEED);
 }
 
+#[allow(dead_code)]
 fn get_vector() -> String {
 
     let md5_hash: String = utils::md5_file(FILE_SEED);
@@ -27,33 +29,37 @@ fn get_vector() -> String {
 
 fn data_operation(encrypt_option: bool, text_str: &str) -> String {
 
-    let secret_key: String = get_key();
-    let secret_iv: String = get_vector();
-
-    // hash
-    let key: &str = &utils::sha256_string(&secret_key)[0..32];
-
-    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
-    let iv: &str = &utils::sha256_string(&secret_iv)[0..16];
-
-    let code_object;
-
-    let cipher = Cipher::aes_256_cbc();
-
-    if encrypt_option {
-
-        code_object = encrypt(cipher, &key.as_bytes(), Some(&iv.as_bytes()), text_str.as_bytes()).unwrap();
-        
-    } else {
-
-        code_object = decrypt(cipher, &key.as_bytes(), Some(&iv.as_bytes()), text_str.as_bytes()).unwrap();
-    }
-
-    let output: String = format!("{:?}", String::from_utf8_lossy(&code_object));
+    // let secret_key: String = get_key();
+    // let secret_iv: String = get_vector();
+    //
+    // // hash
+    // let key: &str = &utils::sha256_string(&secret_key)[0..32];
+    //
+    // // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+    // let iv: &str = &utils::sha256_string(&secret_iv)[0..16];
+    //
+    // let code_object;
+    //
+    // let cipher = Cipher::aes_256_cbc();
+    //
+    // if encrypt_option {
+    //
+    //     code_object = encrypt(cipher, &key.as_bytes(), Some(&iv.as_bytes()), text_str.as_bytes()).unwrap();
+    //
+    // } else {
+    //
+    //     code_object = decrypt(cipher, &key.as_bytes(), Some(&iv.as_bytes()), text_str.as_bytes()).unwrap();
+    // }
+    //
+    // let output: String = format!("{:?}", String::from_utf8_lossy(&code_object));
 
     // println!("{}", output);
 
-    return output;
+    if encrypt_option {
+        return to_string!(text_str);
+    }
+
+    return to_string!(text_str);
 }
 
 pub fn decrypt_operation(str_content: &str) -> String {
